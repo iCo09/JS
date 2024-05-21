7c6758d9042bb297b0fe28570598661d69dfdb40
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-21 19:01:01 +0000

    Learned Datatypes

---
+++ JS_BASICS/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS codes as a newer version of JS
+let name = "rahul";
+let age = 22;
+let logged_in = false;
+
+/* 
+
+Primitive Data Types:
+
+string 
+int
+bool
+null -> object (type)
+undefined -> undefined(type)
+symbol
+
+*/
