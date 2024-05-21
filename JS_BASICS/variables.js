49fc01192c55e8a6ef29bc87143f8942ff0f5dad
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-21 18:46:31 +0000

    variables

---
+++ JS_BASICS/variables.js
@@ -0,0 +1,14 @@
+const Stu_ID = 20309;
+let user_name = "Rahul";
+let address = "Nalagarh";
+
+// AVOID Using Var
+/*
+const -> cant be changed
+let and var -> can be changed
+*/
+
+user_name = "Mahima";
+address = "UP";
+
+console.table([user_name, address]);
