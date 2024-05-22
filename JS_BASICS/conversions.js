bcb9449df000c542a40b49ddd8b1d185f9d3575b
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-22 07:16:53 +0000

    conversions

---
+++ JS_BASICS/conversions.js
@@ -0,0 +1,14 @@
+let age = 22;
+console.log(typeof(age));  //number
+
+let age2 = "22" //string
+console.log(typeof(age2));
+let age3 = Number(age2)   //string to number conversion
+console.log(typeof(age3));
+
+
+/*
+"33" -> 33
+"33abc" -> NaN
+true -> 1,  false -> 0
+*/
