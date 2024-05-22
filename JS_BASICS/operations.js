a4ad59baf6825227eb1ae3e536b70f3528e4e27f
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-22 07:32:09 +0000

    operations

---
+++ JS_BASICS/operations.js
@@ -0,0 +1,11 @@
+let number1 = 3;
+let negNumber = -number1;
+console.log(negNumber);
+
+
+console.log("1" + 2);
+console.log(1 + "2");;
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
