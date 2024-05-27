f5f15ff4f24596acc21a20ac288fefdace3823b9
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-27 19:00:01 +0000

    objects3

---
+++ JS_BASICS2/objects3.js
@@ -0,0 +1,15 @@
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    courseinstructor: "hc"
+}
+
+const {courseinstructor : i} = course //destructuring
+console.log(i);
+
+
+/******JSON******/
+// {
+//     "name": "rahul",
+//     "price": "free"
+// }
