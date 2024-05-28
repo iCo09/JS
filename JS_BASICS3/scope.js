76f5e97f03d479c5ffc9b8d6b83130f3de9a5405
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-28 07:20:11 +0000

    updates

--- JS_BASICS3/scope.js
+++ JS_BASICS3/scope.js
@@ -4,9 +4,54 @@ if(true){
     let a = 10   //block scope
     const b = 20
 
-    console.log("INNER: ", a);
+    //console.log("INNER: ", a);
 }
 
-console.log(a);
+//console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "rahul"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)        
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+//interesting
+
+function addone(num){
+return num+1
+}
+addone(5)
+
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo(5)
+
+
+//This will give error ->
+
+// addTwo(5)
+// const addTwo = function(num){
+//     return num+2
+// }
+
