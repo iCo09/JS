124e4858b7a487b25533bf70699b5198c2fdc50f
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-05-29 06:46:55 +0000

    control floe

---
+++ ControlFlow/one.js
@@ -0,0 +1,32 @@
+ //IF
+const isUserLoggedIn = true
+const temp = 41
+if(temp < 50){
+console.log(`Less Than 50`);
+}
+else{
+console.log(`Greater Than 50`);
+}
+
+// <, >, <=, >=, ==, !=, ===(also check the type), !==
+
+
+//Short notation 
+const bal = 1000
+//if(bal > 500) console.log("test");
+//if(bal > 500) console.log("test"), console.log("test2");
+
+
+//multiple conditions
+if(bal < 500){
+    console.log("less than 500");
+}
+else if(bal < 750){
+    console.log("less than 750");
+}
+else{
+    console.log("less than 1200");
+}
+
+// AND -> &&
+// OR -> || 
