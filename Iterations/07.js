ab068e05249d925f74582ddf11c05155b1c83491
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-06-02 06:34:38 +0000

    map and chaining

---
+++ Iterations/07.js
@@ -0,0 +1,13 @@
+//map function
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.map((num) => num + 10)
+
+//Chaining
+const newNums = myNums
+                .map((num) => num * 10)
+                .map((num) => num + 1)
+
+console.log(newNums);
+
