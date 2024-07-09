82502b68cb6d0a697af2d963d2a1fbb00a3cb007
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-07-09 07:39:36 +0000

    msg

--- Projects/BMI/chaiaurcode.js
+++ Projects/BMI/chaiaurcode.js
@@ -17,6 +17,12 @@ form.addEventListener('submit', function(e){
 
    else{
     const bmi = (weight / ((height*height)/10000)).toFixed(2);
-    results.innerHTML = `<span>${bmi}</span>`;
+    if (bmi < 18.6) {
+        results.innerHTML = `<span>${bmi} Under Weight</span>`;
+    } else if (bmi >= 18.6 && bmi < 24.9) {
+        results.innerHTML = `<span>${bmi} Normal Weight</span>`;
+    } else if (bmi >= 25 && bmi < 29.9) {
+        results.innerHTML = `<span>${bmi} Over Weight</span>`;
+    }
    }
 })
